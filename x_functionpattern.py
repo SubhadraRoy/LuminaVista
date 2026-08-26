35f319f0ef96170b048892f38da5f658b84c3582
Dr. Subhadra Roy <roysubhadra40@gmail.com>
2026-08-26 21:04:42 +0530

    Create x_functionpattern.py

---
+++ x_functionpattern.py
@@ -0,0 +1,20 @@
+def pattern(): 
+    for i in range(3,0,-1): 
+        for k in range(3,i,-1): 
+            print(" ",end="") 
+        for f in range(i): 
+            print("* ",end="") 
+        for k in range(3,i,1): 
+            print("*",end="") 
+        for k in range(i): 
+            print(" ",end="") 
+        print() 
+
+    for i in range(3,0,-1): 
+        for k in range(3,i-1,1): 
+            print(" ",end="") 
+        for f in range(i): 
+            print("*",end="") 
+        print() 
+
+pattern()
