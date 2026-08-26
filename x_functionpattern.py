4dfe6f00cc812fc348f7538bed963789169fe2d9
Dr. Subhadra Roy <roysubhadra40@gmail.com>
2026-08-26 21:32:32 +0530

    .

--- x_functionpattern.py
+++ x_functionpattern.py
@@ -2,29 +2,19 @@ def pattern():
     for i in range(3,0,-1): 
         for k in range(3,i,-1): 
             print(" ",end="") 
-        
-        if i == 3:
-            for f in range(6): 
-                print("* ",end="")
-        elif i == 2:
-            for f in range(2): 
-                print("* ",end="")
-            for f in range(3): 
-                print(" ",end="")
-            for f in range(2): 
-                print("* ",end="")
-        else:
-            for f in range(1): 
-                print("* ",end="")
-            for f in range(5): 
-                print(" ",end="")
-            for f in range(1): 
-                print("* ",end="")
-        
-        print()
+        for f in range(i): 
+            print("* ",end="") 
+        for k in range(3,i,-1): 
+            for j in range(2): 
+                print(" ",end="") 
+        for f in range(i): 
+            print("* ",end="") 
+        print() 
 
     for i in range(3,0,-1): 
-        for k in range(3,i-1,-1): 
+        for k in range(3): 
+            print(" ",end="") 
+        for k in range(3,i,-1): 
             print(" ",end="") 
         for f in range(i): 
             print("* ",end="") 
